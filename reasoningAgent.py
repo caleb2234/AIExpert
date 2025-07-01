70ea8384b4fc5c17ae64963d593857373503a07e
Caleb <ck586@cornell.edu>
2025-07-01 16:45:20 -0400

    fixed data type

--- reasoningAgent.py
+++ reasoningAgent.py
@@ -7,7 +7,7 @@ model_id = "google/medgemma-4b-it"
 
 model = AutoModelForImageTextToText.from_pretrained(
     model_id,
-    torch_dtype = torch.float16,
+    torch_dtype = torch.bfloat16,
     device_map = "auto"
 )
 
@@ -20,14 +20,14 @@ messages = [
         You are an experienced general practitioner tasked to guide a student.
 Use all available information to fill out a JSON on the current state of the case and on the student's progress in the case. You must pinpoint
 three things:
-(1) Which node the student currently is on in the flow chart given their input
-(2) Which node the student currently is on in the flow chart based on the patient conversation history
-(3) List all the skipped nodes that start with Q on the flow chart from ONLY the direct path from patientConversationHistoryNode to the studentInputNode.
+(1) Which node the student currently is on in the flow chart given ONLY their input (studentInputNode)
+(2) Which node the student currently is on in the flow chart based ONLY on the patient conversation history (patientConversationHistoryNode)
+(3) Explain in a few steps what the student missed from the path from the patientConversationHistoryNode to the studentInputNode given the skipped nodes in between, using the flowchart as reference.
 Reply in the following JSON format:
 studentProgress = {
 "studentInputNode" : str # This is for the node given the input
 "patientConversationHistoryNode" : str # This is for the node given the patient conversation history
-"skippedNodes": list[str] # This is for the skipped nodes
+"Explanation": list[str] # This is for the explanation of the skipped nodes in between
                      }
 === Flowchart ===
 Refer to the following flowchart:
@@ -43,7 +43,7 @@ Refer to the following flowchart:
 
 """}]},
 ]
-inputs = reasoning_agent.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_dict = True, return_tensors = 'pt').to(model.device, dtype=torch.float16)
+inputs = reasoning_agent.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_dict = True, return_tensors = 'pt').to(model.device, dtype=torch.bfloat16)
 input_len = inputs["input_ids"].shape[-1]
 with torch.inference_mode():
     generation = model.generate(**inputs, max_new_tokens=1000, do_sample=False)
